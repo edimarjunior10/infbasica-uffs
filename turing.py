5885126c0e55c3d2fd8798b679b658efcd63c33f
fernando <nimbusbrplay@gmail.com>
2023-06-30 16:02:27 -0300

    mudançaaugusto

--- turing.py
+++ turing.py
@@ -1,7 +1,23 @@
-Konrad Ernst Otto Zuse (Berlim, 22 de junho de 1910 — Hünfeld, 18 de dezembro de 1995) foi um engenheiro, inventor e empresário alemão e um pioneiro dos computadores.[1]
+No dia 22 de junho de 2010, o alemão Konrad Zuse completaria 100 anos de vida. O engenheiro berlinense é tido como o criador do primeiro computador do mundo e também da primeira linguagem de programação baseada em algoritmos.
 
-Sua maior conquista foi o primeiro computador elétrico e programável do mundo; o Z3 controlado por programa funcional tornou-se operacional em maio de 1941.[2] Graças a esta máquina e suas predecessoras, Zuse tem sido frequentemente considerado o inventor do computador moderno.[3]
+Entre as contribuições mais conhecidas de Konrad Zuse para o mundo da informática estão as máquinas Z1, Z3 e Z4, construídas respectivamente em 1938, 1941 e 1945. A primeira era uma calculadora com tecnologia mecânica que, segundo a Universidade Técnica (TU) de Berlim, pesava cerca de 500 quilos e era capaz de fazer operações aritméticas de adição, subtração, multiplicação e divisão, além de calcular raiz quadrada e converter decimais em binários e vice-versa.
 
-Zuse ficou conhecido pela máquina de computação S2, considerada o primeiro computador de controle de processo. Em 1941, ele fundou uma das primeiras empresas de computadores, produzindo o Z4, que se tornou o primeiro computador comercial do mundo. De 1943 a 1945 ele projetou o Plankalkül, a primeira linguagem de programação de alto nível. Em 1969, Zuse sugeriu o conceito de um universo baseado em computação em seu livro Rechnender Raum (Calculando o Espaço).[4]
+O desenvolvimento da máquina Z2 em 1939 possibilitou a Konrad Zuse acumular experiência para finalizar, dois anos mais tarde, a construção da Z3. Considerada a primeira calculadora completamente automática e programável em sistema binário do mundo, a invenção tinha todos os componentes encontrados em um computador moderno, exceto memória para armazenar dados ou um programa.
 
-Grande parte de seus primeiros trabalhos foi financiado por sua família e comércio, mas depois de 1939 ele recebeu recursos do governo da Alemanha nazista. Devido à Segunda Guerra Mundial, o trabalho de Zuse passou despercebido no Reino Unido e nos Estados Unidos. Possivelmente, sua primeira influência documentada em uma empresa norte-americana foi a opção da IBM por suas patentes em 1946.[5]
+O Z3 foi concebido antes mesmo do computador britânico Colossus, projetado durante a Segunda Guerra Mundial por Alan Turing, e do estadunidense ENIAC, criado em 1946 por John Eckert e John Mauchly. No entanto, o pesquisador Friedrich Naumann lembra em seu livro Do ábaco à internet: a história da informática (tradução livre) que a invenção de Konrad Zuse não foi levada a sério nem pelo governo nem pela indústria alemães na época.
+Apenas hobby?
+
+Formado em 1935 em Engenharia Civil, depois de passar pelos cursos de Arquitetura e Engenharia Mecânica na TU Berlim, Zuse utilizou a sala da casa dos pais durante anos para conceber os primeiros computadores. O engenheiro também já havia se dedicado à pintura e não tinha conhecimentos específicos de eletricidade ou eletrônica. Isso explica por que, segundo Naumann, a máquina Z3 foi vista durante muito tempo como um brinquedo de Zuse. Segundo o autor, o trabalho do engenheiro era visto apenas como um "hobby".
+No mesmo livro, Naumann relata que nem mesmo Andreas Grohmann, assistente de Zuse, tinha noção do significado de sua contribuição para a construção do Z1. Ele menciona que o assistente teria dito que trabalhava às cegas, "sem saber ao certo como aquele monstro que ali estava deveria funcionar". Entretanto, depois de pronta, a máquina "trabalhava sem grandes problemas e dava soluções exatas para problemas complicados".
+O computador Z4
+
+Em 1945, Konrad Zuse criou o Z4, uma máquina muito mais potente que as anteriores. Enquanto a Z3 e a Z1 levavam entre três e cinco segundos para realizar uma operação de multiplicação, a Z4 era capaz de fazer 11 multiplicações em apenas um segundo. Esses e outros detalhes são divulgados pela TU Berlim em uma exposição realizada em junho de 2010 na Biblioteca Volkswagen, ao lado do campus principal, em homenagem ao ex-aluno da instituição.
+
+A mostra lembra também que, em 1945, Zuse deixou a capital alemã e organizou o transporte do Z4. O Z3 e todas as fotos dele haviam sido destruídos durante um bombardeio aéreo. Durante muitos meses, o Z4 ficou guardado em um galpão na cidade de Hinterstein, próxima à fronteira austríaca.
+Disputa entre potências
+
+A máquina despertou o interesse do matemático Eduard Stiefel. Em setembro de 1945, ele e Zuse assinaram um contrato, formalizando o empréstimo do Z4 ao Instituto de Matemática da Universidade Técnica de Zurique (ETH) por cinco anos.
+
+De acordo com um relatório publicado por aquela universidade em 1981, os suíços pretendiam utilizar o equipamento não só para efetuar cálculos numéricos abrangentes, mas também para enriquecer suas pesquisas e produzir seus próprios computadores em um futuro próximo. Para eles, o empréstimo do Z4 por 30 mil francos suíços era visto como uma boa chance de não ficar de fora na primeira fase da informática.
+
+Além dos Estados Unidos, outras potências europeias vinham apresentando significativos avanços na área, como a Reino Unido e a França. Segundo Naumann, porém, o Z4 foi o único computador da Europa em condições perfeitas de funcionamento até 1951.
