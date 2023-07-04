84649ebf86850cb51ed99646b074fdba213924a6
Thiago <thspanevello@gmail.com>
2023-07-04 18:10:46 -0300

    Mark Zukerberg Thiago

--- turing.py
+++ turing.py
@@ -1,82 +1,27 @@
-def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
-    return mensagem
+# Mark Elliot Zuckerberg (White Plains, 14 de maio de 1984) é um magnata, empresário e filantropo norte-americano, Ele é conhecido por co-fundar o site de mídia social Facebook e sua empresa-mãe Meta Platforms (anteriormente Facebook, Inc.), da qual ele é presidente, diretor executivo e acionista controlador.[4][5] Em março de 2011, a revista Forbes colocou Zuckerberg na 36ª posição da lista das pessoas mais ricas do mundo, com uma fortuna estimada em 17,5 bilhões de dólares.[2] Em junho de 2015, sua fortuna já estava avaliada em 38,4 bilhões de dólares,[1] em 2016 seu patrimônio líquido foi estimado em 51,8 bilhões de dólares.[6]
 
+# Junto aos seus colegas da faculdade da Universidade de Harvard, os estudantes Eduardo Saverin, Andrew McCollum, Dustin Moskovitz e Chris Hughes, lançou o Facebook em 2004.[7][8] O Facebook expandiu-se rapidamente, com um bilhão de usuários até 2012. Zuckerberg foi envolvido em várias disputas legais que foram iniciadas por outros no grupo, que reivindicou uma participação da empresa com base na sua participação durante a fase de desenvolvimento do Facebook.
 
-def doutorado():
-    mensagem = ""
-    return mensagem
+# Em dezembro de 2012, Zuckerberg e sua esposa Priscilla Chan anunciaram que dariam a maior parte de sua riqueza ao longo de suas vidas para "fazer avançar o potencial humano e promover a igualdade" no espírito de The Giving Pledge.[9] Em 01 de dezembro de 2015, eles anunciaram que dariam 99% de suas ações do Facebook (no valor de cerca de 45 bilhões de dólares na época) para a Iniciativa Zuckerberg Chan.[10][11]
 
+# Desde 2010, a revista Time nomeou Zuckerberg entre as 100 pessoas mais ricas e influentes do mundo e também foi nomeado pela revista como a Pessoa do Ano.[12][13] Em dezembro de 2016, Zuckerberg ficou em 10º lugar na lista da Forbes das pessoas mais poderosas do mundo.[14]
 
-def contribuicoes():
-    mensagem = ""
-    return mensagem
+# Em 4 de outubro de 2021, Zuckerberg perdeu cerca de US$ 6 bilhões, em meio a denúncias de uma ex-funcionária da companhia Facebook e à queda do sistema das principais redes do grupo, o Instagram, Messenger e WhatsApp. Essa redução de sua fortuna o fez perder o posto de 4ª pessoa mais rica do mundo, fazendo com que Bill Gates o ultrapassasse.[15] Após perder US$ 29,8 bilhões em fevereiro de 2022, Zuckerberg é a 12° pessoa mais rica do mundo com fortuna de US$ 85,1 bilhões.[16]
 
+# Vida pessoal
+# Mark Elliot Zuckerberg nasceu em White Plains, Condado de Westchester no estado de Nova York, filho de Kristen, uma psiquiatra, e de Edward, um dentista.[2] Mark foi criado em Dobbs Ferry, Condado de Westchester no estado de Nova York, assim como suas três irmãs, Randi, Donna e Arielle.[2] Zuckerberg tem origem judaica, tendo um bar mitzvah, quando ele completou 13 anos,[17][18] embora ele já tenha declarado ser ateu,[18][19] em 2016, ao ser questionado se permanecia descrente, ele respondeu: "Não. Fui criado como judeu e depois passei por um período em questionei as coisas, mas agora acredito que a religião é muito importante”.[20]
 
-def artigos():
-    mensagem = ""
-    return mensagem
+# Na escola onde estudava, a Ardsley High School, teve grande destaque em arte e cultura clássicas. Foi transferido para a Phillips Exeter Academy, onde ganhou vários prêmios em ciências da astronomia, matemática e física. Nos estudos clássicos, Mark aprendeu a ler e escrever francês, hebraico, latim e grego antigo e ainda pertenceu a equipe de esgrima.[21][22][23] Na faculdade, ele era conhecido por recitar versos de poemas épicos como a Ilíada.[21] Em uma festa promovida por sua fraternidade durante seu segundo ano na Universidade Harvard, Zuckerberg conheceu a estudante de medicina Priscilla Chan, sua atual esposa. Em setembro de 2010, Zuckerberg convidou Chan, até então namorada, para morar em sua casa alugada em Palo Alto, Condado de Santa Clara no estado da Califórnia.[2] Zuckerberg começou então a estudar mandarim chinês. O casal visitou a China em dezembro de 2010.[24] Zuckerberg e Chan se casaram em 19 de maio de 2012, um dia após a entrada do Facebook na bolsa de valores Nasdaq.
 
+# Em sua página no Facebook, Zuckerberg listou os seus interesses pessoais como "a abertura, fazendo coisas que ajudam as pessoas a se conectar e compartilhar o que é importante para elas, as revoluções, o fluxo de informações, o minimalismo".[25] Zuckerberg vê azul melhor por causa do daltonismo vermelho-verde, azul também é a cor dominante do Facebook.[26]
 
-def citacoes():
-    mensagem = ""
-    return mensagem
+# Em 2011, uma brecha de segurança do Facebook permitiu que vazassem na Internet fotos que ele configurou como sendo privadas.[27][28][29] As imagens mostram Zuckerberg cozinhando, recebendo amigos e brincando com o cachorro.[30][31][32]
 
+# Em 24 de março de 2023, Zuckerberg anunciou o nascimento de sua filha, Aurelia Zuckerberg, através de suas plataformas de mídia social.[33]
 
-def sair():
-    mensagem = "\nObrigado pela leitura!"
-    return mensagem
+# Desenvolvedor de software
+# Zuckerberg começou a usar computadores e a escrever software ainda adolescente no ensino médio. Seu pai lhe ensinou Atari Programação Básica em 1990 e, posteriormente, contratou o desenvolvedor de software David Newman para lhe dar aulas particulares. Newman não hesitava em chamá-lo de "prodígio", acrescentando que era "duro para ficar à frente dele." Zuckerberg também fez um curso de pós-graduação no assunto no Mercy College perto de sua casa enquanto ele ainda estava no colégio.[2] Ele gostava de criar programas de computador, especialmente as ferramentas de comunicação e jogos. Em um desses programas que criou, surgiu o “ZuckNet”, a qual permitia que todos os computadores entre a casa e o consultório odontológico do seu pai pudessem se comunicar usando o ping para o outro. No que pode ser considerado uma versão simplificada do AOL Instant Messenger, que foi lançado no ano seguinte.[2]
 
+# Algumas crianças brincavam com jogos de computador que Mark criou.[2] Zuckerberg recorda esse período. "Eu tinha um monte de amigos que eram artistas. Eles desenhavam coisas, e eu criava os jogos depois.". Mais tarde ele havia se tornado o capitão da sua escola de preparação de esgrima e obteve um diploma clássico.[2] O co-fundador da Napster, Sean Parker, um amigo próximo, observa que Zuckerberg realmente aprendeu clássicos "de odisseias gregas e todas essas coisas", lembrando que ele já citou as linhas em latim da poesia épica Eneida, de Virgílio, durante uma conferência do Facebook.[2]
 
-def erro():
-    mensagem = "\nOpção inválida!"
-    return mensagem
-
-
-print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
-
-continuar = True
-while continuar == True:
-
-    opcao = int(
-        input(
-"""
-\nDigite o número correspondente ao menu que você deseja acessar:
-1 - Resumo
-2 - Doutorado
-3 - Contribuições
-4 - Principais Artigos
-5 - Citações
-6 - Sair\n
-"""
-        )
-    )
-
-    if opcao == 1:
-        print("1 - Resumo")
-        mensagem = resumo()
-
-    elif opcao == 2:
-        print("2 - Doutorado")
-        mensagem = doutorado()
-
-    elif opcao == 3:
-        print("3 - Contribuições")
-        mensagem = contribuicoes()
-
-    elif opcao == 4:
-        print("4 - Principais Artigos")
-        mensagem = artigos()
-
-    elif opcao == 5:
-        print("5 - Citações")
-        mensagem = citacoes()
-
-    elif opcao == 6:
-        mensagem = sair()
-        continuar = False
-
-    else:
-        mensagem = erro()
-
-    print(mensagem)
+# Durante alguns anos Zuckerberg trabalhou em uma empresa chamada Intelligent Media Group, onde construiu um leitor de música chamado "Synapse Media Player" que usa inteligência artificial para aprender hábitos de escuta do usuário[34] e recebeu a classificação de 3/5 da PC Magazine.[35] A Microsoft e AOL tentaram comprar o Synapse e contratar Zuckerberg, porém ele preferiu se matricular na Universidade Harvard em setembro de 2002.
