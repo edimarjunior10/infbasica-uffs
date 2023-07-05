28677829e39e056fc9e73fe74218f8be766bb040
ManfreedB <manfreedbritzke@hotmail.com>
2023-07-05 19:47:16 -0300

    Trabalho

--- turing.py
+++ turing.py
@@ -1,82 +1,12 @@
-def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
-    return mensagem
+A ideia de Jacquard atravessou o Canal da Mancha, onde inspirou Charles Babbage (1792-1871), um professor de matemática de Cambridge, a desenvolver uma máquina de "tecer números", uma máquina de calcular onde a forma de calcular pudesse ser controlada por cartões.
+Charles Babbage foi um matemático inglês.
+Foi com Charles Babbage que o computador moderno começou a ganhar forma, através de seu trabalho no engenho analítico. O equipamento, apesar de nunca ter sido construído com sucesso, possuía todas as funcionalidades do computador moderno. Foi descrito originalmente em 1837, mais de um século antes que qualquer equipamento do gênero tivesse sido construído com sucesso. O grande diferencial do sistema de Babbage era o fato que seu dispositivo foi projetado para ser programável, item imprescindível para qualquer computador moderno.
+Tudo começou com a tentativa de desenvolver uma máquina capaz de calcular polinômios por meio de diferenças, o calculador diferencial. Enquanto projetava seu calculador diferencial, a ideia de Jacquard fez com que Babbage imaginasse uma nova e mais complexa máquina, o calculador analítico, extremamente semelhante ao computador atual.
 
-
-def doutorado():
-    mensagem = ""
-    return mensagem
-
-
-def contribuicoes():
-    mensagem = ""
-    return mensagem
-
-
-def artigos():
-    mensagem = ""
-    return mensagem
-
-
-def citacoes():
-    mensagem = ""
-    return mensagem
-
-
-def sair():
-    mensagem = "\nObrigado pela leitura!"
-    return mensagem
-
-
-def erro():
-    mensagem = "\nOpção inválida!"
-    return mensagem
-
-
-print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
-
-continuar = True
-while continuar == True:
-
-    opcao = int(
-        input(
-"""
-\nDigite o número correspondente ao menu que você deseja acessar:
-1 - Resumo
-2 - Doutorado
-3 - Contribuições
-4 - Principais Artigos
-5 - Citações
-6 - Sair\n
-"""
-        )
-    )
-
-    if opcao == 1:
-        print("1 - Resumo")
-        mensagem = resumo()
-
-    elif opcao == 2:
-        print("2 - Doutorado")
-        mensagem = doutorado()
-
-    elif opcao == 3:
-        print("3 - Contribuições")
-        mensagem = contribuicoes()
-
-    elif opcao == 4:
-        print("4 - Principais Artigos")
-        mensagem = artigos()
-
-    elif opcao == 5:
-        print("5 - Citações")
-        mensagem = citacoes()
-
-    elif opcao == 6:
-        mensagem = sair()
-        continuar = False
-
-    else:
-        mensagem = erro()
-
-    print(mensagem)
+O projeto, totalmente mecânico, era composto de uma memória, um engenho central, engrenagens e alavancas usadas para a transferência de dados da memória para o engenho central e dispositivos para entrada e saída de dados. O calculador utilizaria cartões perfurados e seria automático.
+Sua parte principal seria um conjunto de rodas dentadas, o moinho, formando uma máquina de somar com precisão de cinquenta dígitos. As instruções seriam lidas de cartões perfurados. Os cartões seriam lidos em um dispositivo de entrada e armazenados, para futuras referências, em um banco de mil registradores. Cada um dos registradores seria capaz de armazenar um número de cinquenta dígitos, que poderiam ser colocados lá por meio de cartões a partir do resultado de um dos cálculos do moinho.
+Por algum tempo, o governo britânico financiou Babbage para construir a sua invenção.
+Além disso tudo, Babbage imaginou a primeira máquina de impressão, que imprimiria os resultados dos cálculos, contidos nos registradores. Babbage conseguiu, durante algum tempo, fundos para sua pesquisa, porém não conseguiu completar sua máquina no tempo prometido e não recebeu mais dinheiro. Hoje, partes de sua máquina podem ser vistas no Museu Britânico, que também construiu uma versão completa, utilizando as técnicas disponíveis na época.
+Durante sua colaboração, a matemática Ada Lovelace publicou os primeiros programas de computador em uma série de notas para o engenho analítico. Por isso, Lovelace é popularmente considerada como a primeira programadora. Em parceria com Charles Babbage, Ada Augusta (1815-1852) ou Lady Lovelace, filha do poeta Lord Byron, era matemática amadora entusiasta. Ela se tornou a pioneira da lógica de programação, escrevendo séries de instruções para o calculador analítico. Ada inventou os conceitos de subrotina, uma seqüência de instruções que pode ser usada várias vezes, loop, uma instrução que permite a repetição de uma sequência de cartões, e do salto condicional, que permite saltar algum cartão caso uma condição seja satisfeita.
+Babbage teve muitas dificuldades com a tecnologia da época, que era inadequada para se construir componentes mecânicos com a precisão necessária. Com a suspensão do financiamento por parte do governo britânico, Babbage e Ada utilizaram a fortuna da família Byron até a falência, sem que pudessem concluir o projeto, e assim o calculador analítico nunca foi construído.
+Ada Lovelace e Charles Babbage estavam avançados demais para o seu tempo, tanto que até a década de 1940, nada se inventou parecido com seu computador analítico. Até essa época foram construídas muitas máquinas mecânicas de somar destinadas a controlar negócios (principalmente caixas registradoras) e algumas máquinas inspiradas na calculadora diferencial de Babbage, para realizar cálculos de engenharia (que não alcançaram grande sucesso).
