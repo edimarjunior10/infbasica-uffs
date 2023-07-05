c247d95e715cf88a86c3fa0cebc38bca986ce714
P3DR0SC <psciesleski@gmail.com>
2023-07-04 22:51:22 -0300

    commit AlunoB

--- turing.py
+++ turing.py
@@ -4,12 +4,12 @@ def resumo():
 
 
 def doutorado():
-    mensagem = ""
+    mensagem = "Linus Benedict Torvalds (Helsínquia, 28 de dezembro de 1969) é um engenheiro de software, nascido na Finlândia e naturalizado estado-unidense em 2010,[1][2] criador, e por muito tempo o desenvolvedor mais importante do núcleo Linux, sendo utilizado em importantes sistemas Linux, Android e Chrome OS. É também o criador do Git, sistema de controle de versão amplamente utilizado, e o aplicativo para planejamento e registro de mergulho, Subsurface.[3]"
     return mensagem
 
 
 def contribuicoes():
-    mensagem = ""
+    mensagem = "Linus define-se como sendo “completamente ateísta religioso”, adicionando: “acredito que as pessoas parecem pensar que a religião traz moral e apreciação a natureza"
     return mensagem
 
 
@@ -19,7 +19,7 @@ def artigos():
 
 
 def citacoes():
-    mensagem = ""
+    mensagem = "Junto ao médico Shinya Yamanaka, foi honrado pela Academia de Tecnologia da Finlândia, em 2012, com o Prêmio de Tecnologia do Milênio “em reconhecimento à sua criação de um novo núcleo de sistema operacional para computadores, que levou ao extensivamente utilizado, núcleo Linux”.[4] Torvalds também notoriamente recebeu os prêmios:"
     return mensagem
 
 
