85fa67d0e3dfc3955d282ba733dd33fc0b42ef50
Wendell Luis Neris <136928757+VandoNeris@users.noreply.github.com>
2023-07-06 17:30:20 -0300

    turing.py

--- turing.py
+++ turing.py
@@ -1,82 +1,66 @@
-def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
-    return mensagem
+Nome completo:Susan Diane Wojcicki
 
+Nacionalidade:Estadunidense
 
-def doutorado():
-    mensagem = ""
-    return mensagem
+Data de nascimento:5 de julho de 1968
 
+Local de nascimento:Condado de Santa Clara, Califórnia, EUA
 
-def contribuicoes():
-    mensagem = ""
-    return mensagem
+Ocupação:Ex-Diretora executiva / Ex-CEO do Youtube
 
+Patrimônio Líquido Estimado:US$ 815 milhões
 
-def artigos():
-    mensagem = ""
-    return mensagem
+Cônjuge:Dennis Troper
 
+Filhos:5 filhos
 
-def citacoes():
-    mensagem = ""
-    return mensagem
+Formação:História e Literatura (Universidade de Harvard)
 
+Quem é Susan Wojcicki?
+Susan Wojcicki é ex-CEO do Youtube, a maior plataforma de compartilhamento de vídeos da Internet. A empresária passou a maior parte de sua carreira no Google, estando no grupo desde a criação da empresa, no final dos anos 1990.
 
-def sair():
-    mensagem = "\nObrigado pela leitura!"
-    return mensagem
+Susan foi fundamental na criação de alguns dos produtos mais lucrativos da história das corporações multinacionais. Monetize o negócio e dê ao Grupo uma vantagem competitiva.
 
+Antes de ingressar no Google, Wojcicki trabalhou na Intel, além da Bain & Company e R.B. Weber.
 
-def erro():
-    mensagem = "\nOpção inválida!"
-    return mensagem
+Trajetória Susan Wojcicki
+Susan Wojcicki é uma executiva de tecnologia norte-americana, conhecida por ser até recentemente a CEO do YouTube desde fevereiro de 2014. Ela é conhecida por ser uma das executivas mais experientes e importantes do setor de tecnologia, tendo passado mais de 20 anos trabalhando no Google.
 
+Formação acadêmica e estudos
+Susan Wojcicki é formada pela Harvard University, onde obteve seu bacharelado em História e Literatura. Ela também possui um MBA da Universidade de Stanford. Durante seus anos universitários, Wojcicki mostrou interesse por jornalismo e escrita, trabalhando como repórter e correspondente do jornal estudantil, e depois como correspondente do Wall Street Journal.
 
-print("\nBoa noite! Você está aprendendo sobre Allan Turing.\n")
+Carreira
+Em 1999, ela se juntou ao Google como chefe de Marketing e foi responsável por alguns dos primeiros acordos publicitários do Google e pelo desenvolvimento de sua plataforma de publicidade.
 
-continuar = True
-while continuar == True:
+Em 2002, ela se mudou para o Google AdSense e, em 2008, para o Google Analytics, onde ajudou a expandir essas unidades de negócios. Em 2009, ela foi nomeada vice-presidente sênior de Produtos e gerenciou o YouTube, Google Books e Google Images.
 
-    opcao = int(
-        input(
-"""
-\nDigite o número correspondente ao menu que você deseja acessar:
-1 - Resumo
-2 - Doutorado
-3 - Contribuições
-4 - Principais Artigos
-5 - Citações
-6 - Sair\n
-"""
-        )
-    )
+Em fevereiro de 2014, Wojcicki foi nomeada CEO do YouTube, onde trabalha para criar novos formatos de conteúdo, atrair mais publicidade e aumentar a presença global do YouTube. Desde então, ela tem liderado o YouTube para se tornar a maior plataforma de vídeo do mundo, com mais de 2 bilhões de usuários ativos mensalmente. Ela deixou a empresa em fevereiro de 2023.
 
-    if opcao == 1:
-        print("1 - Resumo")
-        mensagem = resumo()
+Wojcicki é conhecida por sua liderança forte e visão estratégica e é frequentemente incluída em listas de mulheres poderosas e influentes na tecnologia. Ela também é uma defensora ativa de diversidade e inclusão no setor de tecnologia.
 
-    elif opcao == 2:
-        print("2 - Doutorado")
-        mensagem = doutorado()
+Filantropia
+Susan Wojcicki é conhecida por sua atuação como filantropa e defensora de causas importantes. Ela é membro do conselho de administração de várias organizações de caridade e de carreiras e também tem sua própria fundação. Algumas das suas atividades filantrópicas incluem:
 
-    elif opcao == 3:
-        print("3 - Contribuições")
-        mensagem = contribuicoes()
+Membro do Conselho de Administração do Instituto de Tecnologia de Stanford e do Museu de História de Tecnologia de Silicon Valley.
+Fundou a Fundação Susan Wojcicki, que se concentra em educação e desenvolvimento infantil.
+Membro do Conselho de Administração do Highline Public Schools em Seattle, onde atua como voluntária em programas de educação e desenvolvimento infantil.
+Ela é membro do conselho de administração da Fundação de Educação de Massachusetts e é uma defensora ativa de educação, diversidade e inclusão.
+Wojcicki também tem trabalhado com a organização DonorsChoose.org, que permite que as pessoas doem diretamente para projetos escolares.
+Livros de Susan Wojcicki
+Susan Wojcicki é autora de um livro intitulado "How to Make Advertising Work on the Internet". O livro foi publicado em 1999, quando ela trabalhava na Google e ainda não era amplamente conhecida, mas já apresentava sua visão para a publicidade na internet e seus conhecimentos sobre a indústria.
 
-    elif opcao == 4:
-        print("4 - Principais Artigos")
-        mensagem = artigos()
+Este livro aborda questões como como os anúncios podem ser eficazes na internet e como as empresas podem aproveitar ao máximo as possibilidades da publicidade na web. Este livro é considerado uma peça rara e valiosa para os estudiosos do setor publicitário e dos negócios da tecnologia pois conta com visão e conhecimento de alguém que estava no início de sua carreira e já tinha compreensão da publicidade online.
 
-    elif opcao == 5:
-        print("5 - Citações")
-        mensagem = citacoes()
+Além disso, ela também tem escrito artigos e opiniões publicadas em vários meios de comunicação, como o New York Times, Wall Street Journal e o Harvard Business Review, abordando temas como educação, diversidade, inclusão e questões tecnológicas.
 
-    elif opcao == 6:
-        mensagem = sair()
-        continuar = False
+‍
 
-    else:
-        mensagem = erro()
-
-    print(mensagem)
+Frases de Susan Wojcicki
+"A diversidade e a inclusão são importantes porque aumentam a criatividade e a inovação."
+"Eu acredito que a verdadeira diversidade é a diversidade de perspectivas."
+"A publicidade é uma forma poderosa de atingir as pessoas e transformar suas vidas."
+"A educação é a chave para o sucesso, e isso é especialmente verdadeiro para meninas e mulheres."
+"O YouTube é uma plataforma global incrível, e é uma honra ser a CEO."
+"Nós temos que nos certificar de que a publicidade esteja em um ambiente seguro e de que as pessoas confiem no que estão vendo."
+"É importante que as empresas tenham uma visão mais ampla e que pensam sobre como podem ser responsáveis ​​e contribuir para a sociedade."
+"A tecnologia tem o poder de mudar vidas e transformar comunidades."
